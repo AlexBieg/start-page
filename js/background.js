97c998f3badb6899857d8dc8651b2c05c174d41c
Alex Bieg <alexbieg95@gmail.com>
2019-10-23 12:13:49 -0700

    optimized background animation

--- js/background.js
+++ js/background.js
@@ -23,7 +23,6 @@
     }
 
     drawPoint(ctx) {
-      ctx.fillStyle = '#34b1eb';
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
       ctx.fill();
@@ -31,12 +30,11 @@
 
     drawClosestLine(ctx, points) {
       points.forEach((point) => {
-        const pointDist = Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+        const pointDist = Math.abs(point.x - this.x) + Math.abs(point.y - this.y);
         const percentage = 1 - (pointDist/this.maxDist);
 
         if (pointDist <= this.maxDist) {
-          ctx.strokeStyle = '#34b1eb';
-          ctx.lineWidth = percentage/2;
+          ctx.lineWidth = percentage;
           ctx.beginPath();
           ctx.moveTo(this.x, this.y);
           ctx.lineTo(point.x, point.y);
@@ -47,7 +45,7 @@
   }
 
   const canvas = document.getElementById('background');
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d', { alpha: false });
 
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -64,16 +62,16 @@
     // Determine number of points based on size of screen
     const numPoints = (width / 12) + (height / 12);
     for (let i = 0; i < numPoints; i++) {
-      // Get a randome size
+      // Get a random size
       const size = Math.random() * 2 + 1;
 
       // Set the connection range based on the size of the points
-      const range = (size-1/2-1) * 100 + 50
+      const range = (size-1/2-1) * 160 + 50
       points.push(new Point(
-        Math.random() * (width - 10) + 5,
-        Math.random() * (height - 10) + 5,
-        Math.random() * 1 - .5,
-        Math.random() * 1 - .5,
+        Math.floor(Math.random() * (width - 10) + 5),
+        Math.floor(Math.random() * (height - 10) + 5),
+        Math.random() * .5 - .25,
+        Math.random() * .5 - .25,
         size,
         range
       ))
@@ -81,19 +79,16 @@
   }
 
   const updatePoints = () => {
-    points.forEach(p => {
+    points.forEach((p, index) => {
       p.updatePoint(ctx);
-      p.drawClosestLine(ctx, points);
+      p.drawClosestLine(ctx, points.slice(index));
       p.drawPoint(ctx)
     });
   }
 
   initPoints();
 
-  setInterval(() => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
+  const updateCanvas = () => {
     ctx.canvas.width = width;
     ctx.canvas.height = height;
 
@@ -101,6 +96,11 @@
     ctx.fillStyle = '#444444';
     ctx.fillRect(0, 0, width, height);
 
-    updatePoints()
-  }, 10);
+    ctx.fillStyle = '#34b1eb';
+    ctx.strokeStyle = '#34b1eb';
+    updatePoints();
+    window.requestAnimationFrame(updateCanvas);
+  };
+
+  window.requestAnimationFrame(updateCanvas);
 })();
